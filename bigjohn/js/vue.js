0416efa0991aea542ca6e4134adeceb439057c4a
Dioneya <nikita_kunov8@mail.ru>
2020-05-20 20:10:01 +0300

    Construct Day 2

--- bigjohn/js/vue.js
+++ bigjohn/js/vue.js
@@ -1,11 +1,54 @@
 var app = new Vue({
     el:'#main-info',
     data:{
-        slideActive: 1
+        //Пиццы, список
+        pizzas: [
+            {img: 'img/construcotor-items/pizzas/adreatikojpg.jpg',name: 'Андриатико', price: 299},
+            
+            {img: 'img/construcotor-items/pizzas/adreatikojpg.jpg',name: 'Андриатико', price: 299},
+            
+            {img: 'img/construcotor-items/pizzas/adreatikojpg.jpg',name: 'Андриатико', price: 299},
+        ],
+        //Домашняя кухня, список
+        homeCooking: [
+            {img: 'img/construcotor-items/salade/salade1.jpg',name: 'Домашняя кухня', price: 299},
+            
+            {img: 'img/construcotor-items/salade/salade1.jpg',name: 'Домашняя кухня', price: 299},
+            
+            {img: 'img/construcotor-items/salade/salade1.jpg',name: 'Домашняя кухня', price: 299},
+            
+            {img: 'img/construcotor-items/salade/salade1.jpg',name: 'Домашняя кухня', price: 299},
+            
+            {img: 'img/construcotor-items/salade/salade1.jpg',name: 'Домашняя кухня', price: 299},
+        ],
+        
+        //Салаты, список 
+        salads: [
+            {img: 'img/construcotor-items/salade/salade1.jpg',name: 'Салат', price: 299},
+            
+        ],
+        
+        //Закуски, список
+        snacks: [
+            {img: 'img/construcotor-items/salade/salade1.jpg',name: 'Закуски', price: 299},
+        ],
+        
+        //Десерты, список 
+        
+        desserts: [
+            {img: 'img/construcotor-items/salade/salade1.jpg',name: 'Десерт', price: 299},
+        ],
+        
+        //Барная карта, список
+        
+        drinks:[
+            {img: 'img/construcotor-items/salade/salade1.jpg',name: 'Напиток', price: 299},
+        ]
+        
     },
     methods: {
-        changeSlide(index){
-            app.slideActive = index;
+        set_active(index){
+            mySwiper.slideTo(index-1);
         }
     }
 })
